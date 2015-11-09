62f253ac09c4a29570887e9d05910e717b8c237b
陈翠卓 <chuizhuochen@angejia.com>
2015-11-09 09:48:11 +0800

    fix weichat libs

--- src/app-site/devlibs.task.js
+++ src/app-site/devlibs.task.js
@@ -32,10 +32,31 @@ function task(cb, params) {
         .pipe(rename('highcharts.js'))
         .pipe(gulp.dest(destDir));
 
-    // weichat
-    gulp.src(appDir + './resources/bower/weichat/dist/js/*')
+
+    gulp.src(appDir + './resources/bower/backbone/backbone.js')
         .pipe(gulp.dest(destDir));
+    gulp.src(appDir + './resources/bower/backbone/backbone-min.js')
+        .pipe(rename('backbone.min.js'))
+        .pipe(gulp.dest(destDir));
+    gulp.src(appDir + './resources/bower/underscore/underscore.js')
+        .pipe(gulp.dest(destDir));
+    gulp.src(appDir + './resources/bower/underscore/underscore-min.js')
+        .pipe(rename('underscore.min.js'))
+        .pipe(gulp.dest(destDir));
+    gulp.src(appDir + './resources/bower/nanoscroller/bin/javascripts/jquery.nanoscroller.js')
+        .pipe(gulp.dest(destDir));
+    gulp.src(appDir + './resources/bower/nanoscroller/bin/javascripts/jquery.nanoscroller.min.js')
+        .pipe(gulp.dest(destDir));
+    gulp.src(appDir + './resources/bower/nanoscroller/bin/css/nanoscroller.css')
+        .pipe(gulp.dest(destDir))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss(minifycssOptions))
+        .pipe(gulp.dest(destDir));
+
+    //weichat
     gulp.src(appDir + './resources/bower/weichat/dist/css/*')
+        .pipe(gulp.dest(destDir));
+    gulp.src(appDir + './resources/bower/weichat/dist/js/*')
         .pipe(gulp.dest(destDir));
 
     // angular
@@ -44,6 +65,7 @@ function task(cb, params) {
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest(destDir));
+
 }
 
 module.exports = task;
