f71997a804b0facefe84fbb0e82ae06fc8202af5
陈翠卓 <chuizhuochen@angejia.com>
2015-11-18 19:25:47 +0800

    add nanoscroll lib & deal track.js for requrie

--- src/app-site/devlibs.task.js
+++ src/app-site/devlibs.task.js
@@ -2,6 +2,8 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
+var minifycssOptions = require('../options.config').minifyOptions;
 
 function task(cb, params) {
     var appDir = params.app + '/';
