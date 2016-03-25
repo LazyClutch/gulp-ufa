61e239c0e1e871a2792fddac562cb2b093be0e5e
王鑫卓 <xinzhuowang@angejia.com>
2016-03-25 15:02:25 +0800

    判断非生产环境才会执行sass

--- src/retrx-mgt/sass.task.js
+++ src/retrx-mgt/sass.task.js
@@ -3,20 +3,25 @@
  */
 var gulp = require('gulp');
 var rename = require('gulp-rename');
-var sass = require('gulp-sass');
 
 function task(cb, params) {
     var appDir = params.app + '/';
-    gulp.src(appDir +'resources/assets/sass/**/*.scss')
-        .pipe(sass({
-            includePaths: [appDir + 'resources/bower/foundation/scss'],
-            outputStyle: 'expanded',
-            errLogToConsole: true
-        }).on('error', sass.logError))
-        .pipe(rename('main.css'))
-        .pipe(gulp.dest(appDir + 'resources/assets/src/'));
+    var isProduction = (params.context.env === 'production');
 
-    gulp.watch(appDir + 'resources/assets/scss/**/*.scss', ['sass', 'rem2px', 'deploymain']);
+    if(!isProduction){
+        var sass = require('gulp-sass');
+
+        gulp.src(appDir +'resources/assets/sass/**/*.scss')
+            .pipe(sass({
+                includePaths: [appDir + 'resources/bower/foundation/scss'],
+                outputStyle: 'expanded',
+                errLogToConsole: true
+            }).on('error', sass.logError))
+            .pipe(rename('main.css'))
+            .pipe(gulp.dest(appDir + 'resources/assets/src/'));
+
+        gulp.watch(appDir + 'resources/assets/scss/**/*.scss', ['sass', 'rem2px', 'deploymain']);
+    }
 }
 
 module.exports = task;
