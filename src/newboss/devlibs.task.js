5bd0f2e85ed611e274eed8f8118df1f4454e61d6
刘春晖 <chunhuiliu@angejia.com>
2016-07-23 16:35:18 +0800

    修改libs、main task 去除ie系列文件

--- src/newboss/devlibs.task.js
+++ src/newboss/devlibs.task.js
@@ -1,34 +1,66 @@
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var rename = require('gulp-rename');
-var uglify = require('gulp-uglify');
-var minifycss = require('gulp-minify-css');
+var rename = require('gulp-rename'),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    minifycss = require('gulp-minify-css');
 var minifycssOptions = require('../options.config').minifyOptions;
 
 function task(cb, params) {
     var appDir = params.app + '/';
     var destDir = appDir + 'resources/assets/src/lib';
 
+    // jquery-file-upload
+    gulp.src(appDir + 'resources/bower/jquery-file-upload/js/**/*.js')
+        .pipe(gulp.dest(destDir + '/jquery-file-upload/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest(destDir + '/jquery-file-upload/js'))
+    gulp.src(appDir + 'resources/bower/jquery-file-upload/css/*.css')
+        .pipe(gulp.dest(destDir + '/jquery-file-upload/css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss(minifycssOptions))
+        .pipe(gulp.dest(destDir + '/jquery-file-upload/css'))
+    gulp.src(appDir + 'resources/bower/jquery-file-upload/img/*')
+        .pipe(gulp.dest(destDir + '/jquery-file-upload/img'))
+
+    // jcrop
+    gulp.src(appDir + 'resources/bower/jcrop/js/*')
+        .pipe(gulp.dest(destDir + '/jcrop/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest(destDir + '/jcrop/js'))
+    gulp.src(appDir + 'resources/bower/jcrop/css/*')
+        .pipe(gulp.dest(destDir + '/jcrop/css'))
+
+    // jquery-form-validator
+    gulp.src(appDir + 'resources/bower/jquery-form-validator/form-validator/*')
+        .pipe(gulp.dest(destDir + '/jquery-form-validator'))
+
+    // jquery datetimepicker
+    gulp.src(appDir + 'resources/bower/datetimepicker/*.js')
+        .pipe(gulp.dest(destDir + '/datetimepicker'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest(destDir + '/datetimepicker'))
+
+    gulp.src(appDir + 'resources/bower/datetimepicker/*.css')
+        .pipe(gulp.dest(destDir + '/datetimepicker'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(minifycss(minifycssOptions))
+        .pipe(gulp.dest(destDir + '/datetimepicker'))
 
     //autocomplete
     gulp.src([
-        appDir + 'resources/bower/jquery-ui/ui/core.js',
-        appDir + 'resources/bower/jquery-ui/ui/widget.js',
-        appDir + 'resources/bower/jquery-ui/ui/position.js',
-        appDir + 'resources/bower/jquery-ui/ui/menu.js',
-        appDir + 'resources/bower/jquery-ui/ui/autocomplete.js'
-    ]).pipe(concat('search.box.js'))
+            appDir + 'resources/bower/jquery-ui/ui/core.js',
+            appDir + 'resources/bower/jquery-ui/ui/widget.js',
+            appDir + 'resources/bower/jquery-ui/ui/position.js',
+            appDir + 'resources/bower/jquery-ui/ui/menu.js',
+            appDir + 'resources/bower/jquery-ui/ui/autocomplete.js'
+        ]).pipe(concat('search.box.js'))
         .pipe(gulp.dest(destDir))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest(destDir));
-
-    return gulp.src(appDir + './resources/bower/jquery.lazyload/jquery.lazyload.js')
-        .pipe(gulp.dest(destDir))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest(destDir));
-    
 }
 
 module.exports = task;
--- src/newboss/devmain.task.js
+++ src/newboss/devmain.task.js
@@ -2,51 +2,18 @@ var gulp = require('gulp');
 var importcss = require('gulp-import-css');
 var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
-var browserify = require('browserify');
-var through = require('through2');
 var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
 var minifycss = require('gulp-minify-css');
 var minifycssOptions = require('../options.config').minifyOptions;
-
-var gulpBrowserify = function () {
-    return through.obj(function (chunk, enc, callback) {
-        var self = this;
-
-        var b = browserify(chunk.path).bundle(function (err, buf) {
-            if (err) {
-                console.error('Error:', err.message);
-                return;
-            }
-            chunk.contents = buf;
-            self.push(chunk);
-            callback();
-        });
-    });
-};
 
 function task(cb, params) {
     var appDir = params.app + '/';
     var destDir = appDir + 'resources/assets/src';
 
     gulp.src([
-            appDir + 'resources/assets/src/common/main.js',
-            appDir + 'resources/assets/src/navigation.js',
-            appDir + 'resources/assets/src/common/track.js',
-        ])
-        .pipe(gulpBrowserify())
-        .pipe(rename({suffix: '.dist'}))
-        .pipe(gulp.dest(appDir + 'resources/assets/src/common'));
-
-    /* Main Javascript file. */
-    gulp.src([
-            appDir + 'resources/bower/jquery/dist/jquery.min.js',
-            appDir + 'resources/assets/src/common/main.dist.js',
-            appDir + 'resources/assets/src/common/namespace.js',
-            appDir + 'resources/assets/src/common/common.js',
-            appDir + 'resources/assets/src/common/track.dist.js',
-            appDir + 'resources/assets/src/common/ajax.js',
-            appDir + 'resources/assets/src/common/navigation.dist.js'
+            appDir + 'resources/bower/AUI/dist/js/app.js',
+            appDir + 'resources/assets/src/common/common.js'
         ])
         .pipe(concat('main.js'))
         .pipe(gulp.dest(destDir))
@@ -54,50 +21,12 @@ function task(cb, params) {
         .pipe(uglify())
         .pipe(gulp.dest(destDir));
 
-    // IE
-    gulp.src([
-            appDir + 'resources/bower/jquery/dist/jquery.min.js',
-            appDir + 'resources/assets/src/common/main.dist.js',
-            appDir + 'resources/assets/src/common/namespace.js',
-            appDir + 'resources/assets/src/common/common.js',
-            appDir + 'resources/assets/src/common/track.dist.js',
-            appDir + 'resources/assets/src/common/ajax.js',
-            appDir + 'resources/assets/src/common/navigation.dist.js'
-        ])
-        .pipe(concat('main-ie.js'))
-        .pipe(gulp.dest(destDir))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest(destDir));
-
     /* Main css file. */
     gulp.src([
-            appDir + 'resources/bower/AUI/css/app-platform.css',
-            appDir + 'resources/assets/src/common/navigation.css',
-            appDir + 'resources/assets/src/common/footer.css',
-            appDir + 'resources/assets/src/common/app.css',
-            appDir + 'resources/assets/src/common/iconfont.css',
-            appDir + 'resources/assets/src/common/main.css'
+            appDir + 'resources/bower/AUI/dist/css/app-platform.css',
+            appDir + 'resources/assets/src/common/common.css'
         ])
-        // .pipe(sass({ style: 'expanded' }))
         .pipe(concat('main.css'))
-        .pipe(importcss())
-        .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-        .pipe(gulp.dest(destDir))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss(minifycssOptions))
-        .pipe(gulp.dest(destDir));
-
-    return gulp.src([
-            appDir + 'resources/bower/AUI/css/app-platform.css',
-            appDir + 'resources/assets/src/common/navigation.css',
-            appDir + 'resources/assets/src/common/footer.css',
-            appDir + 'resources/assets/src/common/app-ie.css',
-            appDir + 'resources/assets/src/common/iconfont.css',
-            appDir + 'resources/assets/src/common/main.css'
-        ])
-        // .pipe(sass({ style: 'expanded' }))
-        .pipe(concat('main-ie.css'))
         .pipe(importcss())
         .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
         .pipe(gulp.dest(destDir))
