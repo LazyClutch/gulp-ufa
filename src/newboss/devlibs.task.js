c30a929c180220eb7959718a4d62e2b65f70d5cf
王鑫卓 <xinzhuowang@angejia.com>
2016-07-23 12:07:37 +0800

    删除不用文件

--- src/newboss/devlibs.task.js
+++ src/newboss/devlibs.task.js
@@ -12,62 +12,23 @@ function task(cb, params) {
 
     //autocomplete
     gulp.src([
-            appDir + 'resources/bower/jquery-ui/ui/core.js',
-            appDir + 'resources/bower/jquery-ui/ui/widget.js',
-            appDir + 'resources/bower/jquery-ui/ui/position.js',
-            appDir + 'resources/bower/jquery-ui/ui/menu.js',
-            appDir + 'resources/bower/jquery-ui/ui/autocomplete.js'
-        ]).pipe(concat('search.box.js'))
+        appDir + 'resources/bower/jquery-ui/ui/core.js',
+        appDir + 'resources/bower/jquery-ui/ui/widget.js',
+        appDir + 'resources/bower/jquery-ui/ui/position.js',
+        appDir + 'resources/bower/jquery-ui/ui/menu.js',
+        appDir + 'resources/bower/jquery-ui/ui/autocomplete.js'
+    ]).pipe(concat('search.box.js'))
         .pipe(gulp.dest(destDir))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest(destDir));
 
-    gulp.src(appDir + './resources/bower/jquery.lazyload/jquery.lazyload.js')
+    return gulp.src(appDir + './resources/bower/jquery.lazyload/jquery.lazyload.js')
         .pipe(gulp.dest(destDir))
         .pipe(rename({suffix: '.min'}))
         .pipe(uglify())
         .pipe(gulp.dest(destDir));
-
-    gulp.src(appDir + './resources/bower/highcharts/src/highcharts.min.js')
-        .pipe(gulp.dest(destDir))
-        .pipe(rename('highcharts.js'))
-        .pipe(gulp.dest(destDir));
-
-
-    gulp.src(appDir + './resources/bower/backbone/backbone.js')
-        .pipe(gulp.dest(destDir));
-    gulp.src(appDir + './resources/bower/backbone/backbone-min.js')
-        .pipe(rename('backbone.min.js'))
-        .pipe(gulp.dest(destDir));
-    gulp.src(appDir + './resources/bower/underscore/underscore.js')
-        .pipe(gulp.dest(destDir));
-    gulp.src(appDir + './resources/bower/underscore/underscore-min.js')
-        .pipe(rename('underscore.min.js'))
-        .pipe(gulp.dest(destDir));
-    gulp.src(appDir + './resources/bower/nanoscroller/bin/javascripts/jquery.nanoscroller.js')
-        .pipe(gulp.dest(destDir));
-    gulp.src(appDir + './resources/bower/nanoscroller/bin/javascripts/jquery.nanoscroller.min.js')
-        .pipe(gulp.dest(destDir));
-    gulp.src(appDir + './resources/bower/nanoscroller/bin/css/nanoscroller.css')
-        .pipe(gulp.dest(destDir))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(minifycss(minifycssOptions))
-        .pipe(gulp.dest(destDir));
-
-    //weichat
-    gulp.src(appDir + './resources/bower/weichat/dist/css/*')
-        .pipe(gulp.dest(destDir));
-    gulp.src(appDir + './resources/bower/weichat/dist/js/*')
-        .pipe(gulp.dest(destDir));
-
-    // angular
-    return gulp.src(appDir + './resources/bower/angular/angular.js')
-        .pipe(gulp.dest(destDir))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(uglify())
-        .pipe(gulp.dest(destDir));
-
+    
 }
 
 module.exports = task;
