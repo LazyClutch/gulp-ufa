4f429cee952ad20e92254d2eff10ffe725ccab7b
李韧 <lr_5146@163.com>
2017-10-27 08:07:35 -0500

    Update deploymain.task.js

--- src/tasks/deploymain.task.js
+++ src/tasks/deploymain.task.js
@@ -1,4 +1,11 @@
 var gulp = require('gulp');
+var importcss = require('gulp-import-css');
+var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var minifycss = require('gulp-minify-css');
+var minifycssOptions = require('../options.config').minifyOptions;
 
 function task(cb, params) {
     var appDir = params.app + '/';
@@ -8,6 +15,30 @@ function task(cb, params) {
     var sources = [];
 
     if (isProduction) {
+
+		var devDestDir = appDir + 'resources/assets/src';
+
+		gulp.src([
+			appDir + 'resources/assets/src/common/common.js',
+			appDir + 'resources/assets/src/common/jQuery-ajaxTransport-XDomainRequest.js',
+		])
+			.pipe(concat('main.js'))
+			.pipe(gulp.dest(devDestDir))
+			.pipe(rename({suffix: '.min'}))
+			.pipe(uglify())
+			.pipe(gulp.dest(devDestDir));
+
+        gulp.src([
+			appDir + 'resources/assets/src/common/common.css'
+		])
+			.pipe(concat('main.css'))
+			.pipe(importcss())
+			.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
+			.pipe(gulp.dest(devDestDir))
+			.pipe(rename({suffix: '.min'}))
+			.pipe(minifycss(minifycssOptions))
+			.pipe(gulp.dest(devDestDir));
+
         sources = [
             appDir + 'resources/assets/src/main.min.js',
             appDir + 'resources/assets/src/main.min.css',
